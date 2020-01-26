4c0dc1fbfdcbe927c9496614fec9b0cb86d1de0e
ギワ <tuvshinbayar@mn.data-artist.com>
2020-01-26 16:10:44 +0800

    Done

--- src/provider/api.js
+++ src/provider/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://e.aptek.mn/api/',
-  headers: {'Content-Type': 'application/json'},
+  baseURL: "https://gdsc.warehouse.com/api",
+  headers: { "Content-Type": "application/json" }
 });
 
 export default api;
